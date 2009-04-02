0a823e172b4b283f024b5087348c7cf206ddbb2f
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2009-04-02 12:10:42 +0100

    added local start up page

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,9 +1,9 @@
-pref("browser.startup.homepage", "http://www.google.com");
+pref("browser.startup.homepage", "chrome://viewpoint/content/static/startup.html");
 
 pref("browser.controlport", "7055");
 
 pref("browser.fullscreen", "no");
 
-pref("browser.development", "yes");
+pref("browser.development", "no");
 
 pref("toolkit.defaultChromeURI", "chrome://viewpoint/content/main.xul");
