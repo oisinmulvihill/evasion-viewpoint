9f2ecdc2b737f578458885355b6939df2fba5ae7
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2009-04-05 22:08:37 +0100

    added empty setup file for build process

---
+++ setup.py
@@ -0,0 +1,55 @@
+"""
+Project's setuptool configuration.
+
+This should eggify and in theory upload to pypi without problems.
+
+Oisin Mulvihill
+2009-04-05
+
+"""
+from setuptools import setup, find_packages
+
+Name='evasion-viewpoint'
+ProjecUrl="" #""
+Version='1.0.0'
+Author='Oisin Mulvihill'
+AuthorEmail='oisin dot mulvihill at gmail dot com'
+Maintainer=' Oisin Mulvihill'
+Summary='Internal module used in building of installers.'
+License='tbc'
+ShortDescription=Summary
+Description=Summary
+
+TestSuite = ''
+
+needed = [
+]
+
+EagerResources = [
+]
+
+ProjectScripts = [
+]
+
+PackageData = {
+    # If any package contains *.txt or *.rst files, include them:
+    '': ['*.*'],
+}
+
+setup(
+#    url=ProjecUrl,
+    name=Name,
+    version=Version,
+    author=Author,
+    author_email=AuthorEmail,
+    description=ShortDescription,
+    long_description=Description,
+    license=License,
+    test_suite=TestSuite,
+    scripts=ProjectScripts,
+    install_requires=needed,
+    packages=find_packages('lib'),
+    package_data=PackageData,
+    package_dir = {'': 'lib'},
+    eager_resources = EagerResources,
+)
